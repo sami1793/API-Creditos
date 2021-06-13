a781b1befe1c8b1174f6a856eb31bb5d743ee0bd
sami1793 <sami_17_94@hotmail.com>
2021-06-12 21:50:40 -0300

    Se agregaron notaciones @GetMapping y @PostMapping para poder mapear. Tambien se agregó GenericResponse.

--- src/main/java/ar/com/ada/api/creditos/controllers/ClienteController.java
+++ src/main/java/ar/com/ada/api/creditos/controllers/ClienteController.java
@@ -3,9 +3,14 @@ package ar.com.ada.api.creditos.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import ar.com.ada.api.creditos.entities.Cliente;
+import ar.com.ada.api.creditos.models.GenericResponse;
 import ar.com.ada.api.creditos.services.ClienteService;
 
 @RestController
@@ -14,11 +19,18 @@ public class ClienteController {
     @Autowired
     ClienteService service;
 
-    public List<Cliente> traerTodos(){
-        return service.traerTodos();
+    @GetMapping("/clientes")    
+    public ResponseEntity<List<Cliente>> traerTodos(){
+        return ResponseEntity.ok(service.traerTodos());
     }
 
-    public void crearCliente(Cliente cliente){
-        service.crearCliente(cliente);
+    @PostMapping("/clientes")
+    public ResponseEntity<?> crearCliente(@RequestBody Cliente cliente){  //en vez de void->ResponseEntity<?>
+        service.crearCliente(cliente);//requestBody->viene de front
+        GenericResponse respuesta= new GenericResponse();
+        respuesta.isOk=true;
+        respuesta.id=cliente.getClienteId();
+        respuesta.message="El cliente fue creado con éxito";
+        return ResponseEntity.ok(respuesta);
     }
 }
