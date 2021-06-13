cc152a47e714fa94ea4570229795b4d0ecc3b40c
sami1793 <sami_17_94@hotmail.com>
2021-06-12 21:15:04 -0300

    Se crearon entities, repos, services y controllers

---
+++ src/main/java/ar/com/ada/api/creditos/services/ClienteService.java
@@ -0,0 +1,26 @@
+package ar.com.ada.api.creditos.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.com.ada.api.creditos.entities.Cliente;
+import ar.com.ada.api.creditos.repos.ClienteRepository;
+
+///Vamos a crear un service con la funcionalidad de trater todos los clientes y de grabar un cliente
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    ClienteRepository repository;
+    
+    public List<Cliente> traerTodos(){
+        return repository.findAll();
+    } 
+
+    public void crearCliente(Cliente cliente){
+        repository.save(cliente);
+    }
+}
